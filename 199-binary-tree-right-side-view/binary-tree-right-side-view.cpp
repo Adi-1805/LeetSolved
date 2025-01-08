e580c383d307e4b6b1a7d944ccb4025d43d0f329
Aditi Mishra <145211183+Adi-1805@users.noreply.github.com>
2025-01-09 01:54:54 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (13.88%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -11,16 +11,40 @@
  */
 class Solution {
     // ***********WE WILL DO A ROOT->RIGHT->LEFT traversal**********
-    void traversal(TreeNode* root, int lvl, vector<int>& res){
-        if(root == NULL) return ;
-        if(res.size() == lvl) res.push_back(root->val);
-        traversal(root->right, lvl+1, res);
-        traversal(root->left, lvl+1, res);
-    }
+    // void traversal(TreeNode* root, int& lvl, vector<int>& res){
+    //     if(root == NULL) return ;
+    //     if(res.size() == lvl) res.push_back(root->val);
+    //     traversal(root->right, lvl+1, res);
+    //     traversal(root->left, lvl+1, res);
+    // }
 public:
     vector<int> rightSideView(TreeNode* root) {
+        if(root == NULL) return {};
+        
+        queue <pair<TreeNode*, int>> q; q.push({root, 0});
+        int lvl = 0;
         vector<int> result;
-        traversal(root, 0, result);
+
+        result.push_back(root->val);
+       
+        while(!q.empty()){
+            auto [curNode, lvl] = q.front();
+            q.pop();
+
+            if(result.size() == lvl){
+                result.push_back(curNode->val);
+            }
+            else{
+                result[lvl] = curNode->val;
+            }
+            
+            if(curNode -> left != NULL){
+                q.push({curNode->left, lvl+1});
+            }
+            if(curNode -> right != NULL){
+                q.push({curNode->right, lvl+1});
+            }   
+        }
         return result;
     }
 };
