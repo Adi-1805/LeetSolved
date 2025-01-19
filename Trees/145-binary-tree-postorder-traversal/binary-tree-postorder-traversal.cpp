a11f0ebe915ac1431d4ed4cc5588d015e7c70f63
Aditi Mishra <145211183+Adi-1805@users.noreply.github.com>
2025-01-20 00:04:40 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (18.02%) - LeetSync

--- Trees/145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
+++ Trees/145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
@@ -10,16 +10,36 @@
  * };
  */
 class Solution {
-    void traverse(TreeNode* root, vector<int>& arr){
-        if(root == NULL) return ;
-        traverse(root->left, arr);
-        traverse(root->right, arr);
-        arr.push_back(root->val);
-    }
+    // void traverse(TreeNode* root, vector<int>& arr){
+    //     if(root == NULL) return ;
+    //     traverse(root->left, arr);
+    //     traverse(root->right, arr);
+    //     arr.push_back(root->val);
+    // }
 public:
     vector<int> postorderTraversal(TreeNode* root) {
-        vector<int> postorder;
-        traverse(root, postorder);
-        return postorder;
+        vector<int> result;
+        if (root == nullptr) return result;
+
+        stack<TreeNode*> st1, st2;
+        st1.push(root);
+
+        // use the first stack to traverse the tree in root->right->left order
+        while (!st1.empty()) {
+            TreeNode* curr = st1.top();
+            st1.pop();
+            st2.push(curr);
+
+            if (curr->left) st1.push(curr->left);
+            if (curr->right) st1.push(curr->right);
+        }
+
+        // use the second stack to reverse the order to left->right->root
+        while (!st2.empty()) {
+            result.push_back(st2.top()->val);
+            st2.pop();
+        }
+
+        return result;
     }
 };
