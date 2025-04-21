57bcde9fac7baef29f58f291d75b7c5f2a93b03f
Aditi Mishra <145211183+Adi-1805@users.noreply.github.com>
2025-04-22 01:00:17 +0530

    Time: 12 ms (8.87%) | Memory: 122.5 MB (5.64%) - LeetSync

---
+++ 2249-count-the-hidden-sequences/count-the-hidden-sequences.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numberOfArrays(vector<int>& differences, int lower, int upper) {
+        int n = differences.size();
+        vector<long int> pfsum(n+1,0);
+        // int net_dif = 0;
+        for(int i = 1; i < pfsum.size(); i++){
+            pfsum[i] = pfsum[i-1] + differences[i-1]; 
+            // net_dif += differences[i-1];
+        }
+        long int range = *max_element(pfsum.begin(), pfsum.end()) - *min_element(pfsum.begin(), pfsum.end());
+        long int ans = upper - lower + 1 - range;
+        return  (ans >= 0) ? ans : 0 ;
+
+    }
+};
